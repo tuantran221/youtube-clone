3ff41b6a9b47758a0e56c207571999cfb72e1a59
tuantran221 <trieutuan22012001@gmail.com>
2023-11-15 13:55:49 +0700

    set up structure folder & sass && router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import PageContainer from './layouts/PageContainer'
+import Header from './layouts/Header'
+import Home from './pages/Home'
+import VideoDetail from './pages/VideoDetail'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <PageContainer>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/video" element={<VideoDetail />} />
+          </Routes>
+        </PageContainer>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/layouts/PageContainer/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PageContainer = ({ children }) => {
+  return <div>{children}</div>
+}
+
+export default PageContainer
